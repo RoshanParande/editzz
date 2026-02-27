cf9d5cb37a08466d7066b258b508e59ea74c988e
Roshan Parande <roshanparande9876@gmail.com>
2026-02-27 19:44:54 +0530

    fix CORS for production

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "https://editzzz.vercel.app",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
 
 mongoose
